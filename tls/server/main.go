49c38f7819dfb192e628f76deaf5ebf548349ae4
siddhant <siddhantkumarupmanyu@gmail.com>
2023-06-09 09:59:14 +0530

    server serving certificate

--- tls/server/main.go
+++ tls/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
 	"log"
 )
 
@@ -14,9 +15,19 @@ func main() {
 		log.Fatal(err)
 	}
 	cfg := &tls.Config{Certificates: []tls.Certificate{cert}}
-	listener, err := tls.Listen("tcp", "localhost:14777", cfg)
+	listener, err := tls.Listen("tcp", ":14777", cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = listener
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		_, err = conn.Write([]byte("some data"))
+		if err != nil {
+			return
+		}
+	}
+
 }
